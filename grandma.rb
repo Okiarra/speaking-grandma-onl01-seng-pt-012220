1eb0e4ee28a46e84fe60ecc33e9aea9ce7f98e78
Kiarra Osorio <krosorio2@gmail.com>
2020-02-01 22:34:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,4 +18,6 @@ if phrase == "I LOVE YOU GRANDMA!"
   puts "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == .upcase
   puts "NO, NOT SINCE 1938!"
-else phrase == 
+else
+  puts "HUH?! SPEAK UP, SONNY!"
+end
