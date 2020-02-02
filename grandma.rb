f4629d40d2526c7a824558c2230b318828e0d062
Kiarra Osorio <krosorio2@gmail.com>
2020-02-02 03:05:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -16,9 +16,4 @@ def speak_to_grandma(phrase)
 
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  end
-  elsif phrase == phrase.upcase
-    "NO, NOT SINCE 1938!"
-  else
-    "HUH?! SPEAK UP, SONNY!"
-end
+  
